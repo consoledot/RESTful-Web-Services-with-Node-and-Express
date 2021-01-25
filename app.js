e740946798485e89b41f49548374f4c58e0869b9
consoledot <bimbooladele@outlook.com>
2021-01-25 09:48:19 +0100

    added mongodb database

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 3000
+
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const db = require("./db")
+const port = process.env.PORT || 3000
+const bookRouter = express.Router()
+const Book = require("./models/bookModel")
+db.connect("mongodb://localhost/bookAPI")
+
+bookRouter.route("/books") 
+    .get((req,res)=>{
+
+        res.json({hello:req.query.name, age:req.query.age})
+    })
+
+app.use("/api", bookRouter)
 
 app.get("/", (req,res)=>{
     res.send("Welcome to my API")
